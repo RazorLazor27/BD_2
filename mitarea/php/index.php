2cb3a992ad570293e2c79b458bf5476b75f56afb
Flan <francisca.salinasd@usm.cl>
2023-11-10 18:33:21 -0300

    Nose

--- mitarea/php/index.php
+++ mitarea/php/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
 
   <meta charset="utf-8">
@@ -13,71 +14,44 @@
 
 <body>
 
-  <?php
-  // Tu código PHP va aquí
-  echo "¡Hola! Esto es PHP en acción.";
-  ?>
-
-  <div class="pagina">
-
-    <!-- Cabecera/Menú -->
-    <div class="cabeza">
-
-      <div>
-
-        <a href="index.html">
-          <img title="Sabor USM" src="../images/logo_sin_fondo.png" width="150">
-        </a>
-
-      </div>
-
-      <div class="boton">
-        
-        <a href="top.html">Debería llevarme a top.</a>
-
-      </div>
-
-      <div class="boton">
-
-        <a href="votacion.html">Debería llevarme a votaciones.</a>
-        
-      </div>
-
-      <div class="boton">
-
-        <a href="recetas.html">Debería llevarme a recetas.</a>
-        
-      </div>
-
-      <div class="boton">
-
-        <a href="login.html">Debería llevarme al login.</a>
-
-      </div>
-
+  <!-- Cabecera/Menú -->
+  <div class="cabeza">
+    <div class = "Boton Logo">
+      <a href="index.php">
+        <img title="Sabor USM" src="../images/logo.png" width="158px">
+      </a>
     </div>
-
-    <!-- Contenido de la página, cuerpo completo de la página -->
-    <div class="cuerpo">
-
-      <h1> Menú del día </h1>
-      <p>Aquí tiene que ir la lista con el menú del día xd.</p>
-
-      <br>
-
-      <h1> Menú de la semana </h1>
-      <p>Aquí tiene que el los menús de la semana.</p>
-
+    <div class = "Boton Top_10">
+      <a title="Listados de top 10 de mejores y peores recetas según calificación." href="top.php">Debería llevarme a top.</a>
     </div>
-
-    <!-- Literalmente el pie de página ._. -->
-    <div class="piedepagina">
-
-      <p> footer </p>
-
+    <div class = "Boton Votacion">
+      <a title="Votación Semanal para elegir el plato de fondo de los días Viernes." href="votacion.php">Debería llevarme a votaciones.</a>
     </div>
-
+    <div class = "Boton Usuario">
+      <a title="Inicio de Sesión" href="login.php">Debería llevarme al login.</a>
+    </div>
+    <div class = "Boton Recetas">
+      <a title="Búsqueda de recetas históricas del casino USM" href="recetas.php">Debería llevarme a recetas.</a>
+    </div>
   </div>
+
+  <!-- Contenido de la página, cuerpo completo de la página -->
+  <div class="cuerpo">
+
+    <h1> Menú del día </h1>
+    <p>Aquí tiene que ir la lista con el menú del día xd.</p>
+
+    <br>
+
+    <h1> Menú de la semana </h1>
+    <p>Aquí tiene que el menú de la semana.</p>
+  
+  </div>
+
+  <!-- Literalmente el pie de página ._. -->
+  <footer class="piedepagina">
+    <p>LSKDHASKDHASJDASDHK</p>
+  </footer>
 
 </body>
 
