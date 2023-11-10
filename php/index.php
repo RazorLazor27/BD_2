99607a0603ad07fc7e66ff6bce11a189d9063f85
Flan <francisca.salinasd@usm.cl>
2023-11-10 20:37:04 -0300

    Cambios Flan 20:37

--- php/index.php
+++ php/index.php
@@ -4,6 +4,25 @@
 
     <h1>Menú de hoy</h1>
 
+      <table id="Almuerzo">
+        <tr>
+          <th>Plato de Entrada</th>
+          <td>Platito</td>
+        </tr>
+        <tr>
+          <th>Plato de Fondo</th>
+          <td>Platito</td>
+        </tr>
+        <tr>
+          <th>Postre</th>
+          <td>Platito</td>
+        </tr>
+      </table>
+
+    <br>
+
+    <h1>Menú Semanal</h1>
+
   </div>
 
 <?php include 'base_bottom.php' ?>
