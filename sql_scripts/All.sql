e14aa088b17ec5a05a52dcdcb2b35f58a59d4a96
Flan <francisca.salinasd@usm.cl>
2023-11-10 20:53:52 -0300

    Cambios Flan 20:53

--- sql_scripts/All.sql
+++ sql_scripts/All.sql
@@ -55,11 +55,10 @@ create table recetas (
 
 create table Users(
 	users_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    users_name varchar(60) NOT NULL,
-    users_surname varchar(60) NOT NULL,
+    users_name varchar(255) NOT NULL,
     users_email varchar(255) NOT NULL,
-    users_password varchar(30) NOT NULL,
-    users_num_almuerzos INT NOT NULL,
+    users_password varchar(255) NOT NULL,
+    users_num_almuerzos INT,
     users_login_date DATE,
     users_login_hour TIME
 );
