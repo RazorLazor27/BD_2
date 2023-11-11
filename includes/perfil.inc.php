1d82387f332eedabeb85a496300b600c6b0e1f5a
Nicolas Berenguela <nicolas.berenguela@gmail.com>
2023-11-11 14:19:20 -0300

    Perfil de 0

--- includes/perfil.inc.php
+++ includes/perfil.inc.php
@@ -2,15 +2,15 @@
 //Apretaron el boton para logear, no intentaron meterse a la fuerza
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-    $username = $_POST["username"];
-
-
-
     try {
         require_once "dbh.inc.php";
         require_once "perfil_model.inc.php";
 
         $result = getDatosPerfil($pdo, $username);
+
+        foreach ($result as $key => $value) {
+            echo "$key: $value\n";
+        }
 
         $_SESSION["user_mail"] = $result["email"];
 
--- includes/perfil_model.inc.php
+++ includes/perfil_model.inc.php
@@ -4,7 +4,7 @@
 declare(strict_types= 1);
 
 function getDatosPerfil(object $pdo, string $username){
-    $query = "SELECT * FROM users WHERE user_name = ::username;";
+    $query = "SELECT * FROM users WHERE user_name = :username;";
 
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(":username", $username);
