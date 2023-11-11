4d7241bc8f5e39693eb1d6d60151588274b6a73a
Flan <francisca.salinasd@usm.cl>
2023-11-11 16:40:36 -0300

    FlanCambios 11-11-2023 16:40

--- php/top.php
+++ php/top.php
@@ -1,5 +1,25 @@
 <?php include 'base_top.php' ?>
   <div>
-    <p>Hola soy Top.</p>
+      <h1>Custom Radio Buttons</h1>
+      <label class="container">One
+        <input type="radio" checked="checked" name="radio">
+        <span class="checkmark"></span>
+      </label>
+      <label class="container">Two
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+      </label>
+      <label class="container">Three
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+      </label>
+      <label class="container">Four
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+      </label>
+  </div>
+  <div>
+  </div>
+  <div>
   </div>
 <?php include 'base_bottom.php' ?>
