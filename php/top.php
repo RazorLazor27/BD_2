902dada4f7efe37da7035078073c5e695a4adef1
Flan <francisca.salinasd@usm.cl>
2023-11-12 12:53:33 -0300

    FlanCambios 11-11-2023 21:20

--- php/top.php
+++ php/top.php
@@ -1,26 +1,41 @@
-
 <?php include 'base_top.php' ?>
-  <div>
-      <h1>Custom Radio Buttons</h1>
-      <label class="container">One
-        <input type="radio" checked="checked" name="radio">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container">Two
-        <input type="radio" name="radio">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container">Three
-        <input type="radio" name="radio">
-        <span class="checkmark"></span>
-      </label>
-      <label class="container">Four
-        <input type="radio" name="radio">
-        <span class="checkmark"></span>
-      </label>
+
+  <div id="TOP10">
+
+    <div class="Filtros">
+
+      <h1>Filtros</h1>
+
+      <div>
+
+        <form action="../php/top.php">
+          <label for="tipodeplato">Tipo de plato:</label>
+          <select name="tipodeplato" id="tipodeplato">
+            <option value="nones">Cualquiera</option>
+            <option value="entrada">Plato de Entrada</option>
+            <option value="fondo">Plato de Fondo</option>
+            <option value="postre">Postre</option>
+          </select>
+          <br><br>
+        <input type="checkbox" id="diabetico" name="diabetico" value="diabetico">
+        <label for="vehicle1"> Apto para diabéticos</label><br>
+        <input type="checkbox" id="lactosa" name="lactosa" value="lactosa">
+        <label for="vehicle2"> Apto para intolerantes a la lactosa</label><br>
+        <input type="checkbox" id="gluten" name="gluten" value="gluten">
+        <label for="vehicle3"> Contiene gluten</label><br>
+        <input type="checkbox" id="vegano" name="vegano" value="vegano">
+        <label for="vehicle3"> Apto para veganos</label><br><br>
+        <input type="submit" value="Aplicar">
+        </form>
+
+      </div>
+
+    </div>
+
+    <div>
+
+    </div>
+
   </div>
-  <div>
-  </div>
-  <div>
-  </div>
+
 <?php include 'base_bottom.php' ?>
