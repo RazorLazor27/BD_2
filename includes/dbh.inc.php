82db8cec1a65dfc2f0c5e9b24c316cede6801f15
Nicolas Berenguela <nicolas.berenguela@gmail.com>
2023-11-12 22:56:31 -0300

    asdf

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,12 @@
 <?php
 
 $servername="localhost";
-$username ="admin";
+$username_ad ="admin";
 $password = "!dBeK8jy21r/3nMt";
 $database = "primerabase";
 
 try {
-    $pdo = new PDO("mysql:host=$servename;dbname=$database", $username, $password);
+    $pdo = new PDO("mysql:host=$servename;dbname=$database", $username_ad, $password);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
